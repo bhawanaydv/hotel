6501c806cc4dfa61e12a1281ba260ccbb67253bd
bhawana <bhawanayadav0907@gmail.com>
2025-06-14 16:26:37 +0530

    add firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY,",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // :arrows_anticlockwise: Removed quotes to treat it as an environment variable
   authDomain: "stay-master-ba1e3.firebaseapp.com",
   projectId: "stay-master-ba1e3",
   storageBucket: "stay-master-ba1e3.appspot.com",
@@ -12,4 +12,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth }; // named export
+// Optional: you can still export app if needed
 export default app;
